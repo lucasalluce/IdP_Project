6af12d029aa4a91b2d5e0b4586ce857cff57256c
Luca Salluce <lucasalluce@gmail.com>
2024-12-04 20:35:54 +0100

    Update AutenticationServer.py

--- Back_end/AutenticationServer.py
+++ Back_end/AutenticationServer.py
@@ -9,12 +9,26 @@ dbConnection = mysql.connector.connect(
 dbCursor = dbConnection.cursor()
     # cursor.execute() - funzione del cursore per interagire con il Database
 
-# Users (ID int PK, Name varchar(100), Surname varchar(100), Username varchar(100), PasswordHash varchar(300), Email varchar(200))
+# Users (ID int PK, Name varchar(100), Surname varchar(100), Username varchar(100), HasedPassword varchar(300), Email varchar(200))
 
 class AutenticationServer:
     def __init__(self) -> None:
         pass
-
-    def login (self, jsonUsername, jsonHasedPassword):
-        query = "SELECT PasswordHash FROM Users WHERE Username = %s"
         
+    def login (self, jsonUsername, jsonHasedPassword):
+        query = "SELECT PasswordHash, Email FROM Users WHERE Username = %s"
+        dbCursor.execute(query, (jsonUsername, ))
+        dbReturn = dbCursor.fetchone()
+        
+        for row in dbReturn:
+            if row == None: # Utente inesistenti
+                #TODO Gestione errore - Inesistenza Utente
+                pass
+            elif row == jsonHasedPassword: # Credenziali corrette -> 2FA
+                #TODO 2FA
+                pass
+            elif row != jsonHasedPassword: # Password sbagliata
+                #TODO Gestione errore - Password sbagliata
+                pass
+    
+    del addUser (self, jsonName, jsonSurname, jsonUsername, jsonHasedPassword, jsonEmail)
