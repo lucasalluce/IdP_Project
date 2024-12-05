8ca9d6b9542af70c29bc2b3c198b4a3b9fe3d7e8
Luca Salluce <lucasalluce@gmail.com>
2024-12-05 21:26:34 +0100

    Create AutenticationServer copy.py

---
+++ Back_end/AutenticationServer copy.py
@@ -0,0 +1,59 @@
+# Connessione al database MySQL - IdP_OAuth2_2FA (localhost)
+import mysql.connector
+dbConnection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="IdP_OAuth2_2FA"
+)
+dbCursor = dbConnection.cursor()
+    # cursor.execute() - funzione del cursore per interagire con il database
+
+# Users (ID int PK, Name varchar(100), Surname varchar(100), Username varchar(100), HashedPassword varchar(300), Email varchar(200))
+
+class AutenticationServer:
+    def __init__(self) -> None:
+        pass
+        
+    def login (self, jsonUsername, jsonHashedPassword):
+            # Acquisizione credenziali dal database
+        query = "SELECT HashedPassword, Email FROM Users WHERE Username = %s;"
+        dbCursor.execute(query, (jsonUsername, ))
+        dbReturn = dbCursor.fetchall()
+
+        if len(dbReturn) == 0: # Caso - Utente inseistente
+            pass #TODO Comunicare errore _ Front-end
+        else: # Caso - Utente esistente
+            if dbReturn[0][0] == jsonHashedPassword: # Caso - Credenziali corrette -> protocol2FA()
+                pass #TODO Comunicare primo accesso _ Front-end
+                #TODO protocol2FA() -> MailService
+            else: # Caso - Credenziali sbagliate
+                pass #TODO Comunicare errore _ Front-end
+
+        dbCursor.reset()
+
+    def addUser (self, jsonName, jsonSurname, jsonUsername, jsonHashedPassword, jsonEmail):
+            # Controllo preesistenza Username
+        query = "SELECT Email FROM Users WHERE Username = %s"
+        dbCursor.execute(query, (jsonUsername, ))
+        dbReturn = dbCursor.fetchall()
+
+        if len(dbReturn) == 0: # Caso - Utente da aggiungere
+            query = "INSERT INTO Users (Name, Surname, Username, HashedPassword, Email) VALUES (%s, %s, %s, %s, %s)"
+            dbCursor.execute(query, (jsonName, jsonSurname, jsonUsername, jsonHashedPassword, jsonEmail, ))
+            dbConnection.commit()
+
+            if dbCursor.rowcount > 0: # Caso - Inserimento di un nuovo utente completato
+                pass #TODO Comunicazione utente registrato -> MailService
+            else: # Caso - Errore nell'inserimento
+                pass #TODO Comunicazione errore database - Front_end
+        else: # Caso - Username già utilizzato
+            pass #TODO Comunicazione errore _ Front-end
+                
+        #TODO Verifica di corretto inserimento della nuova tupla nella tabella
+        dbCursor.reset()
+
+# TEST AREA
+#server = AutenticationServer()
+#server.login("l.salluce", "Cifhbab")
+#server.addUser("Mario", "Rossi", "m.rossi", "fdgaffweX", "m.rossi@studenti.poliba.it")
