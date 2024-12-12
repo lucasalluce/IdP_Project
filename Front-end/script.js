9c2f90424eaa419168427f937b940e3e5f9840e4
Luca Salluce <lucasalluce@gmail.com>
2024-12-12 13:23:12 +0100

    Update script.js
    login

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Hashing password
-        // IN clearPassword - password in chiaro acquisita dal form
-        // OUT hashHex - password cifrata con
+    // IN clearPassword - password in chiaro acquisita dal form
+    // OUT hashHex - password cifrata con SHA-256
     async function hashPassword(clearPassword) {
         const encoder = new TextEncoder();
         const data = encoder.encode(clearPassword);                                             // Conversione in binario
@@ -17,50 +17,79 @@ document.addEventListener("DOMContentLoaded", () => {
         otpForm.addEventListener("submit", (e) => {
             e.preventDefault();
             console.log("Modulo inviato");
-            
+
             const otpInput = document.getElementById("otp-input").value;
         });
-    } 
+    }
 
-   // Login
-   const loginForm = document.getElementById("login-form");
-   if (loginForm) {
-       loginForm.addEventListener("submit", (e) => {
-           e.preventDefault();  
+    // Login
+    const loginForm = document.getElementById("login-form");     // Creazione e collegamento al login-form
+    if (loginForm) {
+        loginForm.addEventListener("submit", (e) => {               // Predisposizione all'evento
+            e.preventDefault();                                     // Gestione annullamento dell'evento defautl
+            // Acquisizione campi del login-form
+            const formUsername = loginForm.querySelector("input[type='text']").value;
+            const formPassword = loginForm.querySelector("input[type='password']").value;
+            
+            // Controllo riempimento campi login-form 
+            if (!formUsername || !formPassword) {
+                alert("Inserire correttamente Username e Password");
+                return;
+            }
 
-           const username = loginForm.querySelector("input[type='text']").value;
-           const password = loginForm.querySelector("input[type='password']").value;
+            // Hasing password
+            hashPassword(formPassword).then((hashedPassword) => {           // Acquisizione risposta funzione hashPassword - password cifrata con SHA-256
+                // Chiamata POST HTTP per la creazione del file JSON con i dati del login-forn
+                fetch("http://127.0.0.1:5000/login", {
+                    method: "POST",
+                    headers: {"Contetn-Type": "application/json"},
+                    body: JSON.stringify({                                  // Compilazione file JSON
+                        username: formUsername,
+                        password: hashedPassword,
+                    })
+                })
+                .then((response) => response.json())                        // Acqisizione file JSON di risposta
+                .then((data) => {                                           
+                    if (data.success) {
+                        localStorage.setItem("userEmail", data.email);
+                        window.location.href = "otp.html";
+                    } else {
+                        alert("Credenziali errate!! Riprovare")
+                    }
+                })
+                .catch((error) => {
+                    console.error("Errore: ", error):
+                })
+            });
 
-           //#TODO hashing password  (da fare lato client o lato server, ma qui ci assumiamo che venga fatto lato server)
-
-           fetch("http://127.0.0.1:5000/login", {
-               method: "POST",
-               headers: {
-                   "Content-Type": "application/json",
-               },
-               body: JSON.stringify({
-                   username: username,
-                   password: password,
-               }),
-           })
-               .then((response) => response.json())
-               .then((data) => {
-                   if (data.success) {
-                       window.location.href = "/about3";  // Reindirizza se login riuscito
-                   } else {
-                       alert("Credenziali errate. Riprova.");
-                   }
-               })
-               .catch((error) => {
-                   console.error("Errore:", error);
-               });
-       });
-   }
+            fetch("http://127.0.0.1:5000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.success) {
+                        window.location.href = "/about3";  // Reindirizza se login riuscito
+                    } else {
+                        alert("Credenziali errate. Riprova.");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Errore:", error);
+                });
+        });
+    }
 
 
 
-//--------------------------------------------------------------------------------------------------------------------
-    
+    //--------------------------------------------------------------------------------------------------------------------
+
 
 
 
@@ -109,7 +138,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    
+
     // Gestione password dimenticata
     const forgotPasswordForm = document.getElementById("forgot-password-form");
     if (forgotPasswordForm) {
