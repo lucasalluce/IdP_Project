62fa5d5fef9fdaad9d14f7a6e50f121ea8fba7b4
Luca Salluce <lucasalluce@gmail.com>
2024-12-12 19:26:35 +0100

    Upgrade MailService
    otpMail

--- Back-end/MailService.py
+++ Back-end/MailService.py
@@ -1,2 +1,36 @@
+# Moduli utili
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
-clas
+
+class MailService:
+    def __init__(self):
+        self.senderMail = "idp.mailservice@gmail.com"
+        self.senderPassword = "kohi grli ueyn rupb"
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as self.smtpServer:
+                self.smtpServer.login(self.senderMail, self.senderPassword)
+        except Exception as e:
+            print(f"Errore login riscontrato: {e}")
+        print("MailService - Online")
+    
+    def generateMessage (self, reciverMail, subject, body):
+        mailMessage = MIMEMultipart()
+        mailMessage["From"] = self.senderMail
+        mailMessage["To"] = reciverMail
+        mailMessage["Subject"] = subject
+        mailMessage.attach(MIMEText(body, "plain"))
+        return mailMessage
+    
+    def sendMail (self, mailMessage, reciverMail):
+        try:
+            self.smtpServer.sendmail(self.senderMail, reciverMail, str(mailMessage))
+        except Exception as e:
+            print(f"Errore invio mail riscontrato: {e}")
+    
+    def otpMail (self, otp, reciverMail):
+        body = f"Ecco il tuo personale codice OTP per completare l'accesso: {otp}. Il codice scadrà tra 2 minuti." 
+        mailMessage = self.generateMessage(reciverMail, "2FA protocoll", body)    
+        self.sendMail(mailMessage, reciverMail)
+        
