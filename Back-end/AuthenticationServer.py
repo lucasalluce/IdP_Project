658462962ebcbed3d2530b7bb85b579f8fa9b767
Luca Salluce <lucasalluce@gmail.com>
2024-12-12 20:12:25 +0100

    Update AuthenticationServer.py

--- Back-end/AuthenticationServer.py
+++ Back-end/AuthenticationServer.py
@@ -1,9 +1,9 @@
 # Moduli utili
 # bcrypt - utile al confronto delle hashedPassword
 # random - utile per la generazione dell'OTP
-import bcrypt, random
-
-# Modulo di MailService - utile per l'invio di mail agli utenti
+# time - utile per la validazione dell'OTP
+# MailService - utile per l'invio di mail agli utenti
+import bcrypt, random, time
 from MailService import MailService
 
 # Connessione al database MySQL - IdP_OAuth2_2FA (localhost)
@@ -24,10 +24,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# Gestione OTP
+otpData = []
+
 class AuthenticationServer:
     def __init__(self):
         print("AuthenticationServer - Loading ...")
-        self.mailService = MailService()
+        self.mailService = MailService()        # Inizializzaione MailService
         print("AuthenticationServer - Online, in ascolto ...")
         
     
@@ -37,48 +40,41 @@ class AuthenticationServer:
         otp = random.randint(100000, 999999)
         print("AuthenticationServer.login.2FA - OTP generato: " + str(otp))
         return otp
-    
+
     def login (self, jsonUsername, jsonHashedPassword):
-        
-        
-        print("AuthenticationServer - Inizio della procedura 'login'")
-        
+        print("AuthenticationServer - Inizio procedura 'login'")
         print("AuthenticationServer.login - Interrogazione database ...")
-            # Acquisizione credenziali dal database
+            # Acquisizione possibili risconti dal database
         query = "SELECT HashedPassword, Email FROM Users WHERE Username = %s;"
         dbCursor.execute(query, (jsonUsername, ))
         dbReturn = dbCursor.fetchall()
         dbCursor.reset()
         print("AuthenticationServer.login - Dati acquisiti")
 
-        if len(dbReturn) == 0: # Caso - Username errato/Utente inseistente
+        # Controllo credenziali
+        if len(dbReturn) == 0:  # _Caso_ Username errato/Utente inseistente
             print("AuthenticationServer.login - Nessun riscontro, utente non autenticato")
-            print("AuthenticationServer - Fine della procedura 'login'")
+            print("AuthenticationServer - Fine procedura 'login'")
             return {"success": False, "message": "Username errato/Utente inesistente"} # Risposta del server
-        else: # Caso - Utente esistente
+        else:   # _Caso_ Utente esistente
             dbHashedPassword = dbReturn[0][0]
             dbEmail = dbReturn[0][1]            
             dbReturn.clear()
-            if bcrypt.checkpw(jsonHashedPassword.encode('utf-8'), dbHashedPassword.encode('utf-8')): # Caso - Credenziali corrette -> protocol2FA()
-                print("AuthenticationServer.login - Riscontro totale, utente autenticato")
-                print("AuthenticationServer.login - Inizio alla sotto-procedura '2FA'")
-                otp = self.otpGenerator()
-                self.mailService.otpMail(otp, dbEmail)
+                # _Caso_ Credenziali corrette -> 2FA
+            if bcrypt.checkpw(jsonHashedPassword.encode('utf-8'), dbHashedPassword.encode('utf-8')):    # Controllo corrispondenza password
+                print("AuthenticationServer.login - Riscontro totale, utente autenticato")              
+                print("AuthenticationServer.login - Inizio sotto-procedura '2FA'")
+                otp = self.otpGenerator()                                                               # Generazioni OTP
                 
-                # TODO salvataggio corrispondenza mail-otp,timestamp
-                    # Soluzione di Fede
-                    #otp_data[email] = {
-                    #   "otp": otp,
-                    #   "timestamp": time.time()  # Memorizza il timestamp per scadenza
-                    #}
-                
-                return {"success": True, "message": "Utente verificato, OTP inviato", "Email": dbEmail} # Risposta del server, in allegato mail dell'utente che ha fatto l'accesso (utile per la successiva verifica del codice OTP)
-            else: # Caso - Credenziali sbagliate
+                print("AuthenticationServer.login.2FA - Invio dati al MailService ...")
+                self.mailService.otpMail(otp, dbEmail)                                                  # Invio otpMail - MailService
+                print("AuthenticationServer.login.2FA - In ascolto per la verifica dell'OTP ...")
+                otpData.append({"email": dbEmail, "otp": otp, "timestamp": time.time()})                # Salvataggio dati dell'OTP per la verifica
+                return {"success": True, "message": "Utente verificato, OTP inviato", "email": dbEmail} # Risposta del server, in allegato email dell'utente che ha fatto l'accesso (utile per la successiva verifica del codice OTP)
+            else:   # Caso - Credenziali sbagliate
                 print("AuthenticationServer.login - Riscontro parziale, utente non autenticato")
                 print("AuthenticationServer - Fine della procedura 'login'")
                 return {"success": False, "message": "Password errata"} # Risposta del server
-
-        
 
     def addUser (self, jsonName, jsonSurname, jsonUsername, jsonHashedPassword, jsonEmail):
             # Controllo preesistenza Username
