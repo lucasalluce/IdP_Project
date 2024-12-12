b2fe463a7c67f586ca77926d047509d8b43ede15
Luca Salluce <lucasalluce@gmail.com>
2024-12-12 13:24:10 +0100

    Update script.js

--- Front-end/script.js
+++ Front-end/script.js
@@ -61,37 +61,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     console.error("Errore: ", error):
                 })
             });
-
-            fetch("http://127.0.0.1:5000/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    username: username,
-                    password: password,
-                }),
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.success) {
-                        window.location.href = "/about3";  // Reindirizza se login riuscito
-                    } else {
-                        alert("Credenziali errate. Riprova.");
-                    }
-                })
-                .catch((error) => {
-                    console.error("Errore:", error);
-                });
         });
     }
-
-
-
-    //--------------------------------------------------------------------------------------------------------------------
-
-
-
 
     // Gestione registrazione
     const registerForm = document.getElementById("register-form");
